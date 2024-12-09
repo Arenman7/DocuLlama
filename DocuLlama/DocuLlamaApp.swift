e60e7eb3a398be1a7e36831715ad37f88aa087b5
Aren Baghramian <contact@anbtech.xyz>
2024-12-09 13:45:13 +0100

    Initial Commit

---
+++ DocuLlama/DocuLlamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocuLlamaApp.swift
+//  DocuLlama
+//
+//  Created by Aren Baghramian on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DocuLlamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
